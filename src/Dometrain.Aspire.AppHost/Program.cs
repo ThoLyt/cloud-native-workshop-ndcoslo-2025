93b4957f41a8fa25cc25eccc84acbcee4b745c1a
ThoLyts <thomas.lytskjold@elvia.no>
2025-05-19 14:46:40 +0200

    ex

--- src/Dometrain.Aspire.AppHost/Program.cs
+++ src/Dometrain.Aspire.AppHost/Program.cs
@@ -6,6 +6,7 @@ var mainDbUsername = builder.AddParameter("postgres-username");
 var mainDbPassword = builder.AddParameter("postgres-password");
 
 var mainDb = builder.AddPostgres("main-db", mainDbUsername, mainDbPassword, 5432)
+    .WithImage("postgres:latest")
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent)
     .AddDatabase("dometrain");
@@ -15,8 +16,15 @@ var cartDb = builder.AddAzureCosmosDB("cosmosdb")
     //.RunAsPreviewEmulator()
     .AddCosmosDatabase("cartdb");
 
+var redis = builder.AddRedis("redis")
+    .WithImage("redis:latest")
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithRedisInsight();
+
 builder.AddProject<Projects.Dometrain_Monolith_Api>("dometrain-api")
     .WithReference(mainDb).WaitFor(mainDb)
+    .WithReference(redis).WaitFor(redis)
     .WithReference(cartDb).WaitFor(cartDb);
 
 var app = builder.Build();
