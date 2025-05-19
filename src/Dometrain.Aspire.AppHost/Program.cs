5c0218e7618298af8632afb597addf40b7d34bbb
Nick Chapsas <nick.chapsas@gmail.com>
2025-05-19 11:32:14 +0200

    ex

--- src/Dometrain.Aspire.AppHost/Program.cs
+++ src/Dometrain.Aspire.AppHost/Program.cs
@@ -10,9 +10,14 @@ var mainDb = builder.AddPostgres("main-db", mainDbUsername, mainDbPassword, 5432
     .WithLifetime(ContainerLifetime.Persistent)
     .AddDatabase("dometrain");
 
+
+var cartDb = builder.AddAzureCosmosDB("cosmosdb")
+    //.RunAsPreviewEmulator()
+    .AddCosmosDatabase("cartdb");
+
 builder.AddProject<Projects.Dometrain_Monolith_Api>("dometrain-api")
-    .WithReference(mainDb)
-    .WaitFor(mainDb);
+    .WithReference(mainDb).WaitFor(mainDb)
+    .WithReference(cartDb).WaitFor(cartDb);
 
 var app = builder.Build();
 
